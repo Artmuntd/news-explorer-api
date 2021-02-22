5d2060f0210d60e6b056a4a83d7eaabc9e3b6afc
Artmuntd <69253476+Artmuntd@users.noreply.github.com>
2021-02-22 23:04:26 +0300

    fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,20 +4,53 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const AuthError = require('../errors/auth_err');
 const { tokenNotFoundMessage, unauthorizedMessage } = require('../message');
 
+const handleAuthError = () => {
+  throw new AuthError(unauthorizedMessage);
+};
+
+const extractBearerToken = (header) => header.replace('Bearer ', '');
+
 const getAuth = (req, res, next) => {
-  const { jwt: token } = req.cookies;
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return handleAuthError(res);
+  }
+
+  const token = extractBearerToken(authorization);
+  let payload;
 
   try {
-    if (!token) {
-      throw new AuthError(tokenNotFoundMessage);
-    }
-
-    req.user = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-    next();
+    payload = jwt.verify(
+      token,
+      NODE_ENV === "production" ? JWT_SECRET : 'dev-secret',
+    );
   } catch (err) {
-    throw new AuthError(unauthorizedMessage);
+    return handleAuthError(res);
   }
+
+  req.user = payload;
+
+  return next();
+
 };
+
+// const getAuth = (req, res, next) => {
+//   //const { jwt: token } = req.cookies;
+
+//   const { authorization } = req.headers;
+
+//   try {
+//     if (!token) {
+//       throw new AuthError(tokenNotFoundMessage);
+//     }
+
+//     req.user = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+//     next();
+//   } catch (err) {
+//     throw new AuthError(unauthorizedMessage);
+//   }
+// };
 
 module.exports = {
   getAuth,
